c3a5f0e5032c4c4f5fd6809e7b40fe90eb505627
tamilvelraj <tamilvelraj@gmail.com>
2017-06-09 16:17:08 +0530

    Create TutorialScreenView.h

--- Li2TutorialView/TutorialScreenView.h
+++ Li2TutorialView/TutorialScreenView.h
@@ -1,10 +1,3 @@
-//
-//  TutorialScreenView.h
-//  zohoforms
-//
-//  Created by ZOHOCORP\thamil-1041 on 02/08/13.
-//  Copyright (c) 2013 zohocorp. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
